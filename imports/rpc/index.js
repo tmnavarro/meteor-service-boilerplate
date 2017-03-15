7e5b3f1efe17c99dc339a10fa739ace531a24824
Jake Lacey <jakewlacey@gmail.com>
2017-03-15 23:13:40 +0000

    Service discovery stuff

--- imports/rpc/index.js
+++ imports/rpc/index.js
@@ -1,7 +1,21 @@
-import { Crud } from '../core';
+import { CRUD } from 'meteor/centiq:crud';
+import { Mongo } from 'meteor/mongo';
+import { Class } from 'meteor/jagi:astronomy';
+import { CrudFactory, Service } from '../core';
 import { PingRPC, PingHandler } from './ping';
+
+
+/// PLay
+const ServiceModel = Service.ModelFactory(Mongo, Class);
+
+Service.register(ServiceModel);
+
+const services = Service.GetServices(ServiceModel).fetch();
+
+// play
+
 
 /**
  * Inject the CRUD and PingHandler as dependencies.
  */
-PingRPC(Crud, PingHandler);
+PingRPC(CrudFactory(CRUD), PingHandler);
