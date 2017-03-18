e3245733ad6a7624d643fa01790202bf0601b978
Jake Lacey <jakewlacey@gmail.com>
2017-03-18 20:52:01 +0000

    creating the specification of the ping endpoint

--- features/step_definitions/ping_steps.js
+++ features/step_definitions/ping_steps.js
@@ -1,20 +1,45 @@
+import sinon from 'sinon';
+import { PingService } from '../../imports/rpc/ping';
+
 const { defineSupportCode } = require('cucumber')
+
 
 defineSupportCode(function({Given, When, Then}) {
 
-  Given('the ping service', function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+  Given('the ping service', function () {
+
+    const Crud = {};
+    const ServiceRepository = {
+      registerEndpoint: function() {
+      },
+      networkCall: function() {
+      },
+      services: function() {
+        return { ping: 1 }
+      }
+    };
+
+    this.service = new PingService(Crud, ServiceRepository);
+
   });
 
-  When('I call the endpoint', function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+  When('I call the endpoint', function () {
+
+    // these would need to be stubbed/mocked also.
+    const req = {};
+    const res = {
+      end: function() {
+      }
+    };
+
+    this.service.endpoint(req, res);
+
   });
 
-  Then('return an object', function (callback) {
-    // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+  Then('return an object', function () {
+
+    // Need to inspec service() on res
+
   });
 
 })
